748d0582fbba7d593ae40792d09374db32b0a9fc
lacusrinz <lacusrinz@gmail.com>
2014-03-23 23:03:43 +0800

    fix issue

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,6 +1,6 @@
 from django.forms import widgets
 from rest_framework import serializers
-from snippets .models import Snippet#, LANGUAGE_CHOICES, STYLE_CHOICES
+from snippets.models import Snippet#, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
 # class SnippetSerializer(serializers.Serializer):
